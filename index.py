8cf52064626c804631848c492da7a52ad7d6e416
Tomas <pastoretomas96@gmail.com>
2019-05-22 15:46:29 -0300

    demo tp1

--- index.py
+++ index.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
 
     point1 = Point({'lat': -34.6, 'lng': -58.5}, 'Rodicio', 'Comida rica', "Rodicio", "Restaurantes")
     point2 = Point({'lat': -34.52, 'lng': -58.55}, 'PF Changs', 'Comida asiática', "PF Changs", "Restaurantes")
-    point3 = Point({'lat': -30.52, 'lng': -58.55}, 'Sakiko', 'Comida japonesa', "Sakiko", "Restaurantes")
+    point3 = Point({'lat':-34.55, 'lng': -58.52}, 'Sakiko', 'Comida japonesa', "Sakiko", "Restaurantes")
     point4 = Point({'lat': -34.56, 'lng': -58.4432}, 'Club Amigos', 'Tenis, Football y más', "Club Amigos", "Deportes")
     point5 = Point({'lat': -34.573, 'lng': -58.4548}, 'Future Bar', 'Cerveza artesanal', "Future Bar", "Bares")
 
